cb721ba1a6258db3ba0f5183b1930ec5a1a2ddca
kmtym1998 <kmtym.devloper@gmail.com>
2022-10-25 11:23:40 +0900

    list aliases

--- cmd/list.go
+++ cmd/list.go
@@ -12,9 +12,10 @@ import (
 
 func NewListCmd(ec *cfg.ExecutionContext) *cobra.Command {
 	return &cobra.Command{
-		Use:   "リスト",
-		Short: "登録されているデバイスのリストを表示する",
-		Long:  "登録されているデバイスのリストを表示する",
+		Use:     "ls",
+		Aliases: []string{"list", "リスト", "デバイス一覧"},
+		Short:   "登録されているデバイスのリストを表示する",
+		Long:    "登録されているデバイスのリストを表示する",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			data, err := ec.SwitchBotAPIClient.ListDevices()
 			if err != nil {
