85b8067dc0067e2c5cb1f1ddbd2f41b1e9ef5959
kmtym1998 <kmtym.devloper@gmail.com>
2022-10-28 00:26:20 +0900

    refactor

--- cfg/config_file.go
+++ cfg/config_file.go
@@ -8,13 +8,6 @@ import (
 )
 
 type GlobalCfg struct {
-	DeviceListSize int
-	Token          string
-	Secret         string
-}
-
-// 設定ファイルのスキーマ
-type cfgFile struct {
 	DeviceListSize int    `json:"deviceListSize"`
 	Token          string `json:"token"`
 	Secret         string `json:"secret"`
@@ -26,7 +19,7 @@ func NewGlobalConfig() GlobalCfg {
 
 func (g *GlobalCfg) Prepare(path string) error {
 	if path != "" {
-		if err := g.setConfigFileContentVal(path); err == nil {
+		if err := g.setCfgFileContent(path); err == nil {
 			return nil
 		}
 	}
@@ -40,27 +33,25 @@ func (g *GlobalCfg) Prepare(path string) error {
 	cfgFileName := "config.json"
 	defaultCfgFilePath := filepath.Join(homeDir, cfgDirName, cfgFileName)
 
-	if err := g.setConfigFileContentVal(defaultCfgFilePath); err != nil {
+	if err := g.setCfgFileContent(defaultCfgFilePath); err != nil {
 		return fmt.Errorf("設定ファイルの読み込みに失敗しました。設定ファイルが存在しない または ファイルのスキーマが不正です。実際のエラー: %v", err)
 	}
 
 	return nil
 }
 
-func (g *GlobalCfg) setConfigFileContentVal(path string) error {
+func (g *GlobalCfg) setCfgFileContent(path string) error {
 	b, err := os.ReadFile(path)
 	if err != nil {
 		return err
 	}
 
-	var cfgFileContent cfgFile
+	var cfgFileContent GlobalCfg
 	if err := json.Unmarshal(b, &cfgFileContent); err != nil {
 		return err
 	}
 
-	g.DeviceListSize = cfgFileContent.DeviceListSize
-	g.Token = cfgFileContent.Token
-	g.Secret = cfgFileContent.Secret
+	g = &cfgFileContent
 
 	return nil
 }
